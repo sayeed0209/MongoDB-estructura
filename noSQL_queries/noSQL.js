c10eb344968b66b5cf9ca53095813be8596906fa
abu sayeed <sayeed0209@gmail.com>
2021-05-25 13:12:31 +0200

    noSQL queries

--- noSQL_queries/noSQL.js
+++ noSQL_queries/noSQL.js
@@ -1,12 +1,3 @@
-// Escribe una consulta para mostrar restaurant_id, name, borough y cuisine por todos los documentos en la colección Restaurantes
-// Escribe una consulta para mostrar restaurant_id, name, borough y cuisine, pero excluir el campo _id por todos los documentos en la colección Restaurantes
-// Escribe una consulta para mostrar restaurant_id, name, borough y zip code, pero excluir el campo _id por todos los documentos en la colección Restaurantes
-// Escribe una consulta para mostrar todo los restaurantes que están en el Bronx
-// Escribe una consulta para mostrar los primeros 5 restaurantes que están en el Bronx
-// Escribe una consulta para mostrar el próximo 5 restaurantes después de saltar los primeros 5 del Bronx
-// Escribe una consulta para encontrar los restaurantes que tienen un resultado de más de 90
-// Escribe una consulta para encontrar los restaurantes que tienen un resultado de más que 80 pero menos que 100
-// Escribe una consulta para encontrar los restaurantes cuáles localizan en valor de latitud menos que -95.754168
 // 1
 db.restaurante.find().pretty();
 // 2
@@ -31,6 +22,8 @@ db.restaurante.find({ borough: "Bronx" }).limit(5).pretty();
 //7
 db.restaurante.find({ borough: "Bronx" }).skip(5).limit(5).pretty();
 // 8
+db.restaurante.find({ "grades.score": { $gt: 90 } }).pretty();
 // 9
+db.restaurante.find({ "grades.score": { $gt: 80, $lt: 100 } }).pretty();
 // 10
 db.restaurante.find({ "address.coord": { $lt: -95.754168 } }).pretty();

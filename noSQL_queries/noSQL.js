a177eba7399de5208a0d6505befb6310fbb11d24
abu sayeed <sayeed0209@gmail.com>
2021-05-25 16:28:45 +0200

    query 16

--- noSQL_queries/noSQL.js
+++ noSQL_queries/noSQL.js
@@ -27,3 +27,48 @@ db.restaurante.find({ "grades.score": { $gt: 90 } }).pretty();
 db.restaurante.find({ "grades.score": { $gt: 80, $lt: 100 } }).pretty();
 // 10
 db.restaurante.find({ "address.coord": { $lt: -95.754168 } }).pretty();
+// 11
+db.restaurante
+	.find({
+		$and: [
+			{ cuisine: { $ne: "American " } },
+			{ "grades.score": { $gt: 70 } },
+			{ "address.coord": { $lt: -65.754168 } },
+		],
+	})
+	.pretty();
+// 12
+db.restaurante
+	.find({
+		cuisine: "American ",
+		"grades.score": { $gt: 70 },
+		"address.coord": { $lt: -65.754168 },
+	})
+	.pretty();
+// 13
+db.restaurante
+	.find({
+		$and: [
+			{ borough: { $ne: "Brooklyn" } },
+			{ cuisine: { $ne: "American " } },
+			{ "grades.grade": "A" },
+		],
+	})
+	.sort({ cuisine: -1 })
+	.pretty();
+
+// 14
+db.restaurante.find(
+	{ name: /^Wil/ },
+	{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }
+);
+// 15
+db.restaurante.find(
+	{ name: /ces$/ },
+	{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }
+);
+// 16
+db.restaurante.find(
+	{ name: /.*Reg.*/ },
+	{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }
+);
